34bd8b5b3985451da0144e392cbf1d2baffb8f29
Mariusz Doliński <122319475+mariuszDolinski@users.noreply.github.com>
2023-01-20 21:53:08 +0100

    Add files via upload

---
+++ ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// Odwracanie kolejności w tablicy
+// Autor: Mariusz Doliński
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+string tabS = Console.ReadLine();
+string[] liczbyS = tabS.Split(' ');
+
+for(int i = n-1;i > -1; i--)
+{
+    Console.Write(liczbyS[i]+ " ");
+}
